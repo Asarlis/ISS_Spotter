54e32ceee2e36d5cf6ca7d9b332dd2b8346f1595
Sean Day <Asarlis55@gmail.com>
2021-05-10 12:55:21 +0000

    Made data more accessible before implementing promises

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {fetchMyIP, fetchCoordsByIP,fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
 
 
--- iss.js
+++ iss.js
@@ -3,8 +3,6 @@ const request = require ('request');
 /**
  * Makes a single API request to retrieve the user's IP address.
  * Input:
- *   - A callback (to pass back an error or the IP string)
- * Returns (via Callback):
  *   - An error, if any (nullable)
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
@@ -26,8 +24,6 @@ const fetchMyIP = function(callback) {
  * Makes a single API request to retrieve the lat/lng for a given IPv4 address.
  * Input:
  *   - The ip (ipv4) address (string)
- *   - A callback (to pass back an error or the lat/lng object)
- * Returns (via Callback):
  *   - An error, if any (nullable)
  *   - The lat and lng as an object (null if error). Example:
  *     { latitude: '49.27670', longitude: '-123.13000' }
@@ -55,8 +51,6 @@ const fetchCoordsByIP = function(ip, callback) {
  * Makes a single API request to retrieve upcoming ISS fly over times the for the given lat/lng coordinates.
  * Input:
  *   - An object with keys `latitude` and `longitude`
- *   - A callback (to pass back an error or the array of resulting data)
- * Returns (via Callback):
  *   - An error, if any (nullable)
  *   - The fly over times as an array of objects (null if error). Example:
  *     [ { risetime: 134564234, duration: 600 }, ... ]
@@ -105,11 +99,6 @@ const nextISSTimesForMyLocation = function(callback) {
 
 
 
-module.exports = {
-  fetchMyIP,
-  fetchCoordsByIP,
-  fetchISSFlyOverTimes,
-  nextISSTimesForMyLocation,
-};
+module.exports = { nextISSTimesForMyLocation };
 
 
